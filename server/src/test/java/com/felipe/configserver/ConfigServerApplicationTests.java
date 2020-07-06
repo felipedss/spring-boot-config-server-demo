67790038e06f48e12bdca7a9c28a553b365184c0
felipe <felipe.rosa@projuris.com.br>
2020-07-06 17:03:06 -0300

    Config Server

---
+++ server/src/test/java/com/felipe/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
